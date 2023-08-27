9768296361257301216a23a087901a6530435fc7
Yashkharel19 <yash048339@gmail.com>
2023-08-27 16:51:26 -0400

    ojaswi porfolio first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Ojaswi Rijal</span> I am an accomplished professional, proficient in both interior design and civil engineering. My background in civil engineering provides me with a strong foundation to create aesthetically pleasing and structurally sound spaces. With a keen eye for design and a deep understanding of engineering principles, I craft environments that seamlessly blend beauty and functionality. My passion lies in transforming spaces into captivating experiences, ensuring that every project I undertake is a testament to the perfect fusion of art and science."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
